4ed409e0f54c0a758d71bf5661068d65caddbdfd
LazyCoder48 <ajitesh.newnaha@outlook.com>
2018-10-16 22:49:56 +0530

    initial commit

---
+++ src/main/java/com/techfest/TechFestApplication.java
@@ -0,0 +1,12 @@
+package com.techfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFestApplication.class, args);
+	}
+}
